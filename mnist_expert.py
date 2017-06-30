5e6635638d118b01d77828aeb8bf769f4520b5b9
kuuta326 <r-sasaki@adglobe.co.jp>
2017-06-30 01:38:05 +0000

    updated

--- mnist_expert.py
+++ mnist_expert.py
@@ -28,6 +28,7 @@ def loss(logits, labels):
 
 def convolution(images):
     images = tf.reshape(images, [-1, 28, 28, 1])
+    tf.summary.image('preprocess', images)
 
     output = tf.layers.conv2d(images, filters=32, kernel_size=[5,5], strides=[2,2], padding='SAME')
     output = tf.nn.relu(output)
@@ -78,6 +79,7 @@ def main():
                     print("step %d, training_accuracy %g, train_loss %g" % (i, _accuracy, _loss))
                 if i % 100 == 0 or i == FLAGS.max_step - 1:
                     summary_str = sess.run(summary_op, feed_dict={x: batch[0], y_: batch[1]})
+                    summary_writer.add_summary(summary_str, i)
             
             test_accuracy = sess.run(accuracy_op, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
 
